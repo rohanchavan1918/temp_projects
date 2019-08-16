e4f4379b344976a9e580496901fc74e83fd81d54
Sandeep Mourya <44745276+thund3rstorm@users.noreply.github.com>
2019-08-16 12:20:56 +0530

    NQT8- thund3rstorm

--- tcs-nqt/nqt8.py
+++ tcs-nqt/nqt8.py
@@ -1,3 +1,17 @@
+"""
+Author: thund3rstorm
+
+TCSNQT NINJA CODING
+Write a program to print the final position of a person based on the following:
+The person starts from origin and then,
+he first turns right and travels 10 units of distance
+his second turn is Upward for 20 units
+his third turn is to the left for 30 units
+fourth turn is Downward for 40 units
+fifth turn is right (again) for 50 units....
+and thus he travels, every time increasing the travel distance by 10 units.
+The output position is in form of (x,y) coordinates
+"""
 def newRange(start,end,step):
 	i = start
 	while i < end:
