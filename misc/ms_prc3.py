792b5bb0b67cfe8a7b046583606eae21a7374533
Sandeep Mourya <44745276+thund3rstorm@users.noreply.github.com>
2019-08-19 10:03:40 +0530

    ms_prc3 - thund3rstorm

--- misc/ms_prc3.py
+++ misc/ms_prc3.py
@@ -4,10 +4,10 @@ Author: thund3rstorm
 Given an array of integers, return a new array in which every index carries
 the value of the product of the remaining elements.
 
-Example:	i)	Given array [1,3,2,4,5] it would return
-					Output: [120,40,60,30,24]
-				ii)	Given array [4,10,3] it would return
-					Output: [30,12,40]
+Example: i) Given array [1,3,2,4,5] it would return
+		Output: [120,40,60,30,24]
+	ii) Given array [4,10,3] it would return
+		Output: [30,12,40]
 """
 n=int(input("Enter number of elements\n"))
 el=[]
